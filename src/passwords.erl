7605ccb91b943e11863cd0864703cf11abb8cf2e
Geoff Cant <nem@erlang.geek.nz>
2008-10-22 00:55:04 +0200

    Adding some edoc to ejango.passwords.

--- src/passwords.erl
+++ src/passwords.erl
@@ -26,12 +26,23 @@
 %% API
 %%====================================================================
 
+%% @type password() = #password{}.
+%% An opaque salted/hashed password.
+
+%% @spec new(Plaintext) -> password()
+%% Plaintext = list() | binary()
+%% @doc Creates a new opaque password that can verify the given
+%% plaintext.
 new(Plaintext) when is_list(Plaintext); is_binary(Plaintext) ->
     Salt = salt(),
     #password{hash=hash(?ALGO, Salt, Plaintext),
               salt=Salt,
               algo=?ALGO}.
 
+%% @spec verify(password(), Plaintext) -> bool()
+%% Plaintext = list() | binary()
+%% @doc Verifies that the Plaintext matches the given password. True
+%% if the plaintext is correct.
 verify(P = #password{hash=Hash}, Plaintext) ->
     Hash =:= hash(P, Plaintext).
 
@@ -39,13 +50,19 @@ verify(P = #password{hash=Hash}, Plaintext) ->
 %% Internal functions
 %%====================================================================
 
+%% @private
 hash(#password{algo=Algo,salt=Salt}, Plaintext) ->
     hash(Algo, Salt, Plaintext).
 hash(Algo, Salt, Plaintext) ->
     crypto:Algo(iolist_to_binary([Salt, Plaintext])).
 
+%% @private
 salt() ->
     crypto:rand_bytes(8).
+
+%%====================================================================
+%% Eunit tests
+%%====================================================================
 
 new_test() ->
     ?assertMatch(#password{}, new("Test password")),
