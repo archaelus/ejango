7f039323d849a8cc8b207b3bfb80cc7943ece961
Geoff Cant <nem@erlang.geek.nz>
2009-03-05 17:56:53 +0100

    Improving the edoc for ejango.form_validator.

--- src/ejango/form_validator.erl
+++ src/ejango/form_validator.erl
@@ -30,39 +30,48 @@
 %% API
 %%====================================================================
 
-%% @spec validate(Rules, Data) -> [Result]
-%% Rules = [form_validation_rule()]
-%% Data = [{Key::string(), Value::string()}]
-%% Result = {Key::string(), [predicate_result()]}
+%% @type validation() = {RuleNmae::string(), [predicate_result()]}.
+%% @type validation_result() = [validation()].
+
+%% @type form_data() = [{FieldName::string(), FieldValue::string()}].
+
+%% @spec validate([rule()], form_data()) -> validation_result()
 validate(Rules,Data) when is_list(Rules), is_list(Data) ->
     lists:map(fun (Rule) -> validate_rule(Rule, Data) end,
               Rules).
 
+%% @spec invalid_fields(validation_result()) -> field()
 invalid_fields(Results) ->
     [Field || {Field, Errors} <- Results,
               length(Errors) >= 1].
 
+%% @spec is_valid(validation_result()) -> bool()
 is_valid(Results) ->
     invalid_fields(Results) =:= [].
 
+%% @spec has_error(validation_result()) -> bool()
 has_error(Results) ->
     is_valid(Results) =:= false.
 
+%% @spec field_errors(validation_result(), field()) -> validation()
 field_errors(Name, Results) ->
     case lists:keysearch(Name, 1, Results) of
         false -> erlang:error(no_such_field);
         {value, V} -> V
     end.
 
+%% @spec errors(validation_result()) -> field()
 errors(Results) ->
     [Field || Field = {_, Errors} <- Results,
               length(Errors) >= 1].
 
+%% @spec valid_fields(validation_result(), form_data()) -> form_data()
 valid_fields(Results, Data) ->
     Fields = [ Field || {Field, []} <- Results],
     [ {K,V} || {K,V} <- Data,
                lists:member(K, Fields) ].
 
+%% @spec validate_rule(rule(), form_data()) -> validation()
 validate_rule({Name, Predicates}, Data) when is_list(Name), is_list(Predicates) ->
     {Name,
      lists:flatmap(fun (Predicate) ->
@@ -77,8 +86,41 @@ validate_rule({Name, Predicates}, Data) when is_list(Name), is_list(Predicates) 
 %% Internal functions
 %%====================================================================
 
-%% @spec validate_predicate(Predicate, Name, Data) -> true | false | {error, Reason}
-%% @doc Checks a single condition in a rule.
+%% @type field() = string().
+%% A field name.
+
+%% @type rule() = {Name, [predicate()]}.
+%%  where Name = fields() | string()
+%% A form validation rule. Most predicates assume the name of the rule
+%% is also the name of the field to validate.
+
+%% @type predicate() = {'duplication', [field()]} |
+%%                     'not_empty' |
+%%                     'string' |
+%%                     {'length', LengthSpec} |
+%%                     {'predicate', function()} |
+%%                     {'not_predicate', function()} |
+%%                     'email_address' |
+%%                     {'regex', Regex::string()} |
+%%                     {'member', [term()]}.
+%% where
+%%     LengthSpec = [Exact::integer()] | [Min::integer(), Max::integer()].
+%%
+%% A test that determines the validity of field values.
+%% <ul>
+%% <li>{'duplication', [field()]} requires that the list of fields given have the same value.</li>
+%% <li>not_empty - the value must not be missing or the empty string</li>
+%% <li>string - the value must not be missing</li>
+%% <li>length - reqires that the value is either exactly Exact chars long or between Min and Max inclusive.</li>
+%% <li>{predicate, Fun} - requrires Fun(Value) to return true</li>
+%% <li>{not_predicate, Fun} - requrires Fun(Value) to return false</li>
+%% <li>email_address - requires the value to validate as an email address</li>
+%% <li>{regex, Regex} - requires that the value match the regex (using the rvre engine)</li>
+%% <li>{member, List} - requires that the value be a member of the given List.</li>
+%% </ul>
+
+%% @spec validate_predicate(predicate(), field(), Data) -> true | false | {error, Reason}
+%% @doc Checks a single predicate in a rule.
 %% @end
 validate_predicate({duplication, [Field|Duplicates]}, _Name, Data)
   when is_list(Duplicates), length(Duplicates) >= 1 ->
@@ -132,6 +174,7 @@ same_value(Field, FieldValue, [Duplicate|Rest], Data) ->
         _ -> {error, {different_value, Field, Duplicate}}
     end.
 
+%% @spec rule_fields(rule()) -> field()
 rule_fields({Name, Rules}) ->
     lists:usort(lists:append([ predicate_fields(Name, Rule)
                                || Rule <- Rules ])).
